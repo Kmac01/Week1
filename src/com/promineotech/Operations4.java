3835c8593a8d19fcbfd6c73e057409fafc0f98ec
Kmac01 <117133875+Kmac01@users.noreply.github.com>
2022-12-14 04:08:53 -0500

    Week1 assignment

---
+++ src/com/promineotech/Operations4.java
@@ -0,0 +1,10 @@
+package com.promineotech;
+
+public class Operations4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/promineotech/VariableInitial.java
@@ -0,0 +1,18 @@
+package com.promineotech;
+
+public class VariableInitial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char middleInitial = 'J';
+		
+		
+		checkType(middleInitial);
+	}
+
+	private static void checkType(char middleInitial) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
